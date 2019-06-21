044ba32d0e2d69d0e8b3a2ed8295bdeb11ff9e5c
Kenneth.Wang <785172550@qq.com>
2019-06-21 19:25:16 +0800

    Create ServerStarter.java

---
+++ src/main/java/com/wh/start/ServerStarter.java
@@ -0,0 +1,57 @@
+package com.wh.start;
+
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
+
+/*
+
+系统设计： 
+* 
+* 详情页 (CDN 缓存) -> 系统时间 -> 倒计时js 
+*   |
+*   v
+* 地址暴露接口 (后端优化)
+*   |
+*   V
+* 执行秒杀操作 (后端优化)
+*   |
+*   V
+* 返回结果
+* 
+* -------------
+* 
+*  秒杀操作 事务  网络延迟 GC
+*  update 减库存
+*  insert 购买明细
+*  commit
+*/
+
+/**
+ * @ClassName: ServerStarter
+ * @author: hw83770
+ * @desc this is a simple demo of seckill web application
+ * 
+ */
+public class ServerStarter {
+
+	public static void main(String[] args) {
+		System.out.println("Server starting ...");
+
+		VertxOptions options = new VertxOptions();
+		options.setEventLoopPoolSize(8);
+		Vertx vertx = Vertx.vertx(options); // create vertx app
+
+		DeploymentOptions depOps = new DeploymentOptions();
+		depOps.setInstances(8);
+		vertx.deployVerticle(SeckillVerticle.class, depOps, ar -> { // deploy a verticle(an event loop app)
+			if (ar.succeeded()) {
+				System.out.println("done deployment");
+			} else {
+				System.out.println(ar.cause());
+			}
+		});
+
+	}
+
+}
